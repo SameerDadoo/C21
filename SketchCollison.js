fbe33f6c0189898301a78501dafda64f60fb5784
SameerDadoo <70300053+SameerDadoo@users.noreply.github.com>
2020-09-11 19:56:32 -0500

    Add files via upload

---
+++ SketchCollison.js
@@ -0,0 +1,45 @@
+var fixedRect, movingRect;
+
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+ 
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 
+    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2 
+    && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) 
+    { movingRect.shapeColor = "red"; fixedRect.shapeColor = "red"; } 
+    else {
+         movingRect.shapeColor = "green"; fixedRect.shapeColor = "green"; 
+        }
+
+
+ 
+  drawSprites();
+}
+
+function isTouching(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2
+    && object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+    
+    return true;
+  }
+  else {
+    return false;
+  } 
+}
